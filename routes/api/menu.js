5e652a1e83d784a10602c812316dc2897117ec4a
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-24 12:49:35 +0300

    Update menu.js

--- routes/api/menu.js
+++ routes/api/menu.js
@@ -5,5 +5,6 @@ const { menu: ctrl } = require('../../controllers');
 const router = express.Router();
 
 router.get('/:menuId', ctrl.getShopMenu);
+router.post('/', ctrl.addMenuItem);
 
 module.exports = router;
