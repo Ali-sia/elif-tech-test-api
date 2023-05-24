c9131cc93fe8aa204f6aa4b964570f244faed634
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-24 15:24:22 +0300

    update: order routs

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,6 +4,7 @@ const { orders: ctrl } = require('../../controllers');
 
 const router = express.Router();
 
+router.get('/', ctrl.getAllOrders);
 router.post('/', ctrl.addOrder);
 
 module.exports = router;
