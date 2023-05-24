bd1a38ca92b51c66c95e3696920460d7f20db416
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-24 11:17:36 +0300

    create: menu orders controllers structure

---
+++ controllers/menu/getShopMenu.js
@@ -0,0 +1,26 @@
+// const { Contact } = require('../../models/index');
+// const createError = require('http-errors');
+
+const { catchAsync } = require('../../utils/index');
+
+const getShopMenu = catchAsync(async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const { _id } = req.user;
+
+//   const result = await Contact.findById(contactId, _id).populate(
+//     'owner',
+//     '_id name email'
+//   );
+
+//   if (!result) {
+//     throw createError(404, `Contact with id ${contactId} not found`);
+//   }
+
+//   res.json({
+//     status: 'success',
+//     code: 200,
+//     data: { result },
+//   });
+});
+
+module.exports = getShopMenu;
