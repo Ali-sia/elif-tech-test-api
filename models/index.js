8d2ce42f2d2843e4ebd636d590f97b49b2e7e4fe
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-24 15:24:57 +0300

    update: index model imoprts

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
-const Menu = require("./menu");
-const Shop = require("./shop");
+const Menu = require('./menu');
+const Shop = require('./shop');
+const Order = require('./order');
 
 module.exports = {
   Menu,
   Shop,
+  Order,
 };
