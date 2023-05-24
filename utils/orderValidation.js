f63bdfc1c9863368f229c3b658a642eef26b3a86
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-24 11:33:52 +0300

    create: order joi validator

---
+++ utils/orderValidation.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+const orderValidator = data => {
+  const schema = Joi.object({
+    client: Joi.object({
+      name: Joi.string().min(3).max(40).allow(''),
+      email: Joi.string().email({ minDomainSegments: 2 }),
+      phone: Joi.string().min(3).max(16).allow(''),
+      address: Joi.string().min(10).max(160).allow(''),
+    }),
+    menuItem: Joi.array(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = orderValidator;
